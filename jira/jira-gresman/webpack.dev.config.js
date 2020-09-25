16454ce677783787837286991db3005b6b4461d9
Viarshadau Dzianis <dzianis_viarshadau@senla.eu>
2020-09-26 01:03:18 +0300

    Change tuning files.

--- jira/jira-gresman/webpack.dev.config.js
+++ jira/jira-gresman/webpack.dev.config.js
@@ -7,7 +7,9 @@ module.exports = {
     devtool: 'inline-source-map',
     entry: {
         'background/background': path.join(__dirname, './src/background/background.js'),
-        'content/content': path.join(__dirname, './src/content/content.js'),
+        'content/dashboard': path.join(__dirname, './src/content/dashboard.js'),
+        'content/secure': path.join(__dirname, './src/content/secure.js'),
+        'popup/popup': path.join(__dirname, './src/popup/popup.js'),
     },
     output: {
         path: path.join(__dirname, 'build'),
@@ -18,6 +20,8 @@ module.exports = {
         new CopyWebpackPlugin([
             {from: './src/manifest.json', to: 'manifest.json'},
             {from: './src/icons', to: 'icons'},
+            {from: './src/background', to: 'background'},
+            {from: './src/popup', to: 'popup'},
         ])
     ]
 };
